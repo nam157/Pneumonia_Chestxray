db58e086d91e0d03da3c2ef1e28b1c2258219ed4
username=nam157 <nguyenhoainamhuongkhe2@gmail.com>
2023-03-06 23:27:13 +0700

    add training

--- core/train.py
+++ core/train.py
@@ -1,1 +1,91 @@
+import torch
+import time
+from tqdm import tqdm
+from torch.cuda.amp import autocast,GradScaler
+import numpy as np
+scaler = GradScaler()
 
+def train_one_epoch(epoch, model, loss_fn, optimizer, train_loader, device, scheduler=None, schd_batch_update=False):
+    model.train()
+
+    t = time.time()
+    running_loss = None
+
+    pbar = tqdm(enumerate(train_loader), total=len(train_loader))
+    for step, (imgs, image_labels) in pbar:
+        imgs = imgs.to(device).float()
+        image_labels = image_labels.to(device).long()
+        with autocast():
+            image_preds = model(imgs) 
+
+            loss = loss_fn(image_preds, image_labels)
+            
+            scaler.scale(loss).backward()
+
+            if running_loss is None:
+                running_loss = loss.item()
+            else:
+                running_loss = running_loss * .99 + loss.item() * .01
+
+            if ((step + 1) %  10 == 0) or ((step + 1) == len(train_loader)):
+                # may unscale_ here if desired (e.g., to allow clipping unscaled gradients)
+
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad() 
+                
+                if scheduler is not None and schd_batch_update:
+                    scheduler.step()
+
+            if ((step + 1) % 100 == 0) or ((step + 1) == len(train_loader)):
+                description = f'epoch {epoch} loss: {running_loss:.4f}'
+                
+                pbar.set_description(description)
+                
+    if scheduler is not None and not schd_batch_update:
+        scheduler.step()
+
+def valid_one_epoch(epoch, model, loss_fn, val_loader, device, best_val, fold, scheduler=None, schd_loss_update=False):
+    model.eval()
+
+    t = time.time()
+    loss_sum = 0
+    sample_num = 0
+    image_preds_all = []
+    image_targets_all = []
+    
+    pbar = tqdm(enumerate(val_loader), total=len(val_loader))
+    for step, (imgs, image_labels) in pbar:
+        imgs = imgs.to(device).float()
+        image_labels = image_labels.to(device).long()
+        
+        image_preds = model(imgs)   #output = model(input)
+        #print(image_preds.shape, exam_pred.shape)
+        image_preds_all += [torch.argmax(image_preds, 1).detach().cpu().numpy()]
+        image_targets_all += [image_labels.detach().cpu().numpy()]
+        
+        loss = loss_fn(image_preds, image_labels)
+        
+        loss_sum += loss.item()*image_labels.shape[0]
+        sample_num += image_labels.shape[0]  
+
+        if ((step + 1) % 100 == 0) or ((step + 1) == len(val_loader)):
+            description = f'epoch {epoch} loss: {loss_sum/sample_num:.4f}'
+            pbar.set_description(description)
+    
+    image_preds_all = np.concatenate(image_preds_all)
+    image_targets_all = np.concatenate(image_targets_all)
+    valid_acc = (image_preds_all==image_targets_all).mean()
+    if (valid_acc > best_val):
+        best_val = valid_acc
+        torch.save(model.state_dict(), f'./best_model_fold{fold}.pt')
+        
+    print('validation multi-class accuracy = {:.4f}'.format(valid_acc))
+    
+    if scheduler is not None:
+        if schd_loss_update:
+            scheduler.step(loss_sum/sample_num)
+        else:
+            scheduler.step()
+    
+    return best_val
